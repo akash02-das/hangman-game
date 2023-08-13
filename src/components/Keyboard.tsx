08734f61e081490ae611ac289b1511009dbf37cb
akash das <aku.das02@gmail.com>
2023-08-14 00:28:31 +0600

    design keyboard layout

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,5 +1,23 @@
+import { KEYS } from '../constants/keys';
+
 const Keyboard = () => {
-  return <div>Keyboard</div>;
+  return (
+    <div className='self-stretch'>
+      <div
+        className='grid gap-2'
+        style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))' }}
+      >
+        {KEYS.map((key) => (
+          <button
+            key={key}
+            className='w-full bg-none text-gray-800 border-solid border-2 border-gray-800 text-4xl uppercase p-2 font-bold cursor-pointer rounded-lg shadow-lg hover:bg-slate-300 active'
+          >
+            {key}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Keyboard;
