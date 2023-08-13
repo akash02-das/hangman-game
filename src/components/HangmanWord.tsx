2afa4f9e2ed508ba286f71830d1993c3285b58bb
akash das <aku.das02@gmail.com>
2023-08-13 23:16:18 +0600

    design hangman guessed word

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,5 +1,25 @@
 const HangmanWord = () => {
-  return <div>HangmanWord</div>;
+  const word = 'hangman';
+  const guessedLetters = ['g', 'n', 'h', 'f'];
+
+  return (
+    <div className='flex text-5xl font-bold uppercase font-letterFont gap-4'>
+      {word.split('').map((letter, index) => (
+        <span
+          key={index}
+          className='text-gray-800 border-solid border-b-8 border-gray-800'
+        >
+          <span
+            className={`${
+              guessedLetters.includes(letter) ? 'visible' : 'invisible'
+            }`}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default HangmanWord;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
     },
     fontFamily: {
       appFont: ['Montserrat', 'sans-serif'],
+      letterFont: ['DM Mono', 'monospace'],
     },
   },
   plugins: [],
