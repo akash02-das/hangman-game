c3e4a297b22ad729294b61b33b467781be1f4cf3
akash das <aku.das02@gmail.com>
2023-08-14 02:52:40 +0600

    add keyboard logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import './App.css';
 import HangmanDrawing from './components/HangmanDrawing';
@@ -14,12 +14,46 @@ function App() {
     (letter) => !wordToGuess.includes(letter)
   );
 
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter)) return;
+
+      setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+    },
+    [guessedLetters]
+  );
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+
+    document.addEventListener('keypress', handler);
+
+    return () => {
+      document.removeEventListener('keypress', handler);
+    };
+  }, [guessedLetters, addGuessedLetter]);
+
+  const activeLetters = guessedLetters.filter((letter) =>
+    wordToGuess.includes(letter)
+  );
+
   return (
     <div className='max-w-4xl flex items-center flex-col gap-8 mx-auto'>
       <div className='text-4xl text-center'>Lose Win</div>
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
       <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
-      <Keyboard />
+      <Keyboard
+        activeLetters={activeLetters}
+        inactiveLetters={incorrectLetters}
+        addGuessedLetter={addGuessedLetter}
+      />
     </div>
   );
 }
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,20 +1,39 @@
 import { KEYS } from '../constants/keys';
 
-const Keyboard = () => {
+type KeyboardProps = {
+  activeLetters: string[];
+  inactiveLetters: string[];
+  addGuessedLetter: (letter: string) => void;
+};
+
+const Keyboard = ({
+  activeLetters,
+  inactiveLetters,
+  addGuessedLetter,
+}: KeyboardProps) => {
   return (
     <div className='self-stretch'>
       <div
         className='grid gap-2'
         style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))' }}
       >
-        {KEYS.map((key) => (
-          <button
-            key={key}
-            className='w-full bg-none text-gray-800 border-solid border-2 border-gray-800 text-4xl uppercase p-2 font-bold cursor-pointer rounded-lg shadow-lg hover:bg-slate-300 active'
-          >
-            {key}
-          </button>
-        ))}
+        {KEYS.map((key) => {
+          const isActive = activeLetters.includes(key);
+          const isInactive = inactiveLetters.includes(key);
+
+          return (
+            <button
+              key={key}
+              onClick={() => addGuessedLetter(key)}
+              className={`w-full bg-none text-gray-800 border-solid border-2 border-gray-800 text-4xl uppercase p-2 font-bold cursor-pointer rounded-lg shadow-lg hover:bg-slate-300 ${
+                isActive ? 'active' : ''
+              } ${isInactive ? 'inactive' : ''}`}
+              disabled={isInactive || isActive}
+            >
+              {key}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
